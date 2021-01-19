43840478922dc0bfc5b527276e92230e63129806
Henry VIEN <72934249+auchanVN@users.noreply.github.com>
2021-01-19 10:10:21 +0000

    Ajout de la BDD
    Modification des pages de vérification mail pour ajouter la BDD

--- Verification_mail/page.php
+++ Verification_mail/page.php
@@ -3,28 +3,6 @@
 <html lang='fr'>
     <head>
         <title>Vérification d'email</title>
-        <?php
-        if (isset($_POST['envoyer'])) {
-            $email = $_POST['mailform'];
-            $header="Mine-Version: 1.0\r\n";
-            $header.='From: "Contact.SCHBOTZ.com"<support@schobtz'."\n";
-            $header.='Content-Type:text/html; charset="utf-8"'."\n";
-            $header.='Content-Transfer-Encoding: 8bit';
-
-            $message ='
-            <!DOCTYPE html>
-            <html lang="fr">
-                <body>
-                    <div style="padding:0;margin:0 auto;font-size:16px;font-weight:normal;line-height:22px"> 
-                        There’s one quick step you need to complete before creating your account. Let’s make sure this is the right email address for you — please confirm this is the right address to use for your new account. <br/>
-                        <a href="http://localhost/verified.php?confirmation=yes&email='.$email.'">Verify here</a>
-                    </div>                
-                </body>
-            </html>';
-        
-            mail($email,"Please verify your email address",$message,$header);
-        }
-        ?>
         <?php 
             $dBHost = "localhost";
             $dBUser = "root";
@@ -32,19 +10,48 @@
             $dBDatabase = "schobz_data";
             $port = "3306";
 
+
             $conn = mysqli_connect($dBHost, $dBUser, $dBPassword, $dBDatabase, $port);
 
             if (!$conn) {
                 die("Échec de la connexion:" . mysqli_connect_error());
             } 
         ?>
+        <?php
+        if (isset($_POST['envoyer'])) {
+            $username = $_POST['nom'];
+            $email = $_POST['mailform'];
+            $header="Mine-Version: 1.0\r\n";
+            $header.='From: "Contact.SCHBOTZ.com"<support@schobtz'."\n";
+            $header.='Content-Type:text/html; charset="utf-8"'."\n";
+            $header.='Content-Transfer-Encoding: 8bit';
+            if (isset($username,$email)){
+            $message ='
+            <!DOCTYPE html>
+            <html lang="fr">
+                <body>
+                    <div style="padding:0;margin:0 auto;font-size:16px;font-weight:normal;line-height:22px"> 
+                        Bonjour,'.$username.'. Merci de votre inscription dans la formation. Cependant, il reste une dernière étape avant la validation de votre compte. Il suffit de cliquer sur le lien ci-joint:<br/>
+                        <a href="http://localhost/verified.php?verified=1&nom='.$username.'&email='.$email.'">Verify here</a>
+                    </div>                
+                </body>
+            </html>';
+        
+            mail($email,"Une dernière étape,".$username,$message,$header);
+            } else {
+                echo "<p>Merci de bien renseigner vos informations</p>";
+            }
+        }
+        ?>
+        
     </head>
     <body>
         <h1>
             Merci de votre inscription. Cependant, il reste une dernière étape. <br/>
             Il vous reste à vérifier votre adresse mail. Merci de renseigner votre addresse mail.
         </h1>
-        <form method="POST" action="">
+        <form method="POST">
+            <input type="nom" placeholder="Renseigner votre nom" name="nom">
             <input type="email" placeholder="Recevoir un mail" name="mailform"> <!-- Permet d'envoyer un mail en un click -->
             <input type="submit" name="envoyer">
         </form>
--- Verification_mail/verified.php
+++ Verification_mail/verified.php
@@ -2,11 +2,31 @@
 <html>
     <body>
         <?php
-            $verification = $_GET['confirmation'];
-            if ($verification == 'yes') {
-                echo "Confirmé";
+            $verification = $_GET['verified'];
+            if ($verification == 1) {
+                $nom =$_GET['nom'];
+                $dBHost = "localhost";
+                $dBUser = "root";
+                $dBPassword = "root";
+                $dBDatabase = "schobz_data";
+                $port = "3306";
+
+                $conn = mysqli_connect($dBHost, $dBUser, $dBPassword, $dBDatabase, $port);
+
+                if (!$conn) {
+                    die("Échec de la connexion:" . mysqli_connect_error());
+                }
+                
+                $sql = 'UPDATE personne SET verified=1 WHERE (SELECT nom FROM personne)='.$nom;
+
+                if ($conn->query($sql) === TRUE) {
+                    echo "Compte vérifié";
+                  } else {
+                    echo "Une erreur s'est produite lors de la vérification du compte: " . $conn->error;
+                  }
+                  $conn->close();
             } else { 
-                echo "Pas confirmé"; 
+                echo "Nous sommes désolés. Il semble y avoir une erreur lors de la vérification. Merci de réessayer"; 
             }
             
         ?>
